6f7a1afd5ab69399a143e5e39a687f7ba8be9fd8
DanArmor <danarmor301330@gmail.com>
2023-03-21 01:12:29 +0300

    feat: testing cli main

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/DanArmor/GoTorrent/pkg/torrent"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatal("Not enough args")
+	}
+	inPath := os.Args[1]
+	outPath := os.Args[2]
+
+	tf, err := torrent.Parse(inPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tf.DownloadToFile(outPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
