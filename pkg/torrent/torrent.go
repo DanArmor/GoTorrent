9f036cef9ba313b77404d5997dde711ad51d81db
DanArmor <danarmor301330@gmail.com>
2023-03-21 01:11:42 +0300

    feat: torrent downloading functions

--- pkg/torrent/torrent.go
+++ pkg/torrent/torrent.go
@@ -4,13 +4,20 @@ import (
 	"bytes"
 	"crypto/sha1"
 	"fmt"
+	"math/rand"
+	"net/http"
 	"net/url"
 	"os"
 	"strconv"
+	"time"
 
-	"github.com/jackpal/bencode-go"
+	"github.com/DanArmor/GoTorrent/pkg/p2p"
+	"github.com/DanArmor/GoTorrent/pkg/peers"
 	"github.com/DanArmor/GoTorrent/pkg/utils"
+	"github.com/jackpal/bencode-go"
 )
+
+const Port uint16 = 6881
 
 type bencodeTorrent interface {
 	toTorrentFile() (TorrentFile, error)
@@ -18,9 +25,14 @@ type bencodeTorrent interface {
 
 type bencodeInfoV1 struct {
 	Pieces      string `bencode:"pieces"`
-	PieceLength int  `bencode:"piece length"`
-	Length      int  `bencode:"length"`
+	PieceLength int    `bencode:"piece length"`
+	Length      int    `bencode:"length"`
 	Name        string `bencode:"name"`
+}
+
+type bencodeTrackerRespCompact struct {
+	Interval int    `bencode:"interval"`
+	Peers    string `bencode:"peers"`
 }
 
 type bencodeTorrentV1 struct {
@@ -61,12 +73,12 @@ func (bt *bencodeTorrentV1) toTorrentFile() (TorrentFile, error) {
 		return TorrentFile{}, nil
 	}
 	t := TorrentFile{
-		Announce: bt.Announce,
-		InfoHash: infoHash,
+		Announce:    bt.Announce,
+		InfoHash:    infoHash,
 		PieceHashes: pieceHashes,
 		PieceLength: bt.Info.PieceLength,
-		Length: bt.Info.Length,
-		Name: bt.Info.Name,
+		Length:      bt.Info.Length,
+		Name:        bt.Info.Name,
 	}
 	return t, nil
 }
@@ -75,8 +87,8 @@ type TorrentFile struct {
 	Announce    string
 	InfoHash    [utils.InfoHashLen]byte
 	PieceHashes [][utils.PieceHashLen]byte
-	PieceLength int64
-	Length      int64
+	PieceLength int
+	Length      int
 	Name        string
 }
 
@@ -101,14 +113,74 @@ func (tf *TorrentFile) buildTrackerURL(peerID [20]byte, port uint16) (string, er
 		return "", err
 	}
 	params := url.Values{
-		"info_hash" : []string{string(tf.InfoHash[:])},
-		"peer_id" : []string{string(peerID[:])},
-		"port" : []string{strconv.Itoa(int(port))},
-		"uploaded" : []string{"0"},
-		"downloaded" : []string{"0"},
-		"compact" : []string{"1"},
-		"left" : []string{strconv.Itoa(tf.Length)},
+		"info_hash":  []string{string(tf.InfoHash[:])},
+		"peer_id":    []string{string(peerID[:])},
+		"port":       []string{strconv.Itoa(int(port))},
+		"uploaded":   []string{"0"},
+		"downloaded": []string{"0"},
+		"compact":    []string{"1"},
+		"left":       []string{strconv.Itoa(tf.Length)},
 	}
 	base.RawQuery = params.Encode()
 	return base.String(), nil
+}
+
+func (tf *TorrentFile) requestPeers(peerID [utils.PeerIDLen]byte, port uint16) ([]peers.Peer, error) {
+	url, err := tf.buildTrackerURL(peerID, port)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &http.Client{Timeout: 15 * time.Second}
+	resp, err := c.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	trackerResp := bencodeTrackerRespCompact{}
+	err = bencode.Unmarshal(resp.Body, &trackerResp)
+	if err != nil {
+		return nil, err
+	}
+	return peers.Unmarshal([]byte(trackerResp.Peers))
+}
+
+func (tf *TorrentFile) DownloadToFile(path string) error {
+	var peerID [utils.PeerIDLen]byte
+	_, err := rand.Read(peerID[:])
+	if err != nil {
+		return err
+	}
+	peers, err := tf.requestPeers(peerID, Port)
+	if err != nil {
+		return err
+	}
+	torrent := p2p.Torrent{
+		Peers:       peers,
+		PeerID:      peerID,
+		InfoHash:    tf.InfoHash,
+		PieceHashes: tf.PieceHashes,
+		PieceLength: tf.PieceLength,
+		Length:      tf.Length,
+		Name:        tf.Name,
+	}
+
+	buf, err := torrent.Download()
+	if err != nil {
+		return err
+	}
+
+	outFile, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	_, err = outFile.Write(buf)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
